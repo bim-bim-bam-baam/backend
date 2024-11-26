735599874d4ed71298f2a19c925f82f15d4e3680
sour-soup <kiziyepi8@gmail.com>
2024-11-26 23:08:34 +0400

    init commit

---
+++ src/main/java/org/bimbimbambam/hacktemplate/HackTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.bimbimbambam.hacktemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackTemplateApplication.class, args);
+    }
+
+}
